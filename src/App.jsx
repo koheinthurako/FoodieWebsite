e1c0bbcbe6fa9239628108c40663f296a5041fd4
koheinthurako <captainthura99@gmail.com>
2023-10-28 23:36:53 +0630

    start navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import {IoRestaurant} from "react-icons/io5"
 const App = () => {
   return (
     <div className="container mx-auto">
-      <IoRestaurant/>
+      <div className="flex items-center gap-3 my-8">
+        <IoRestaurant className="text-2xl text-pink-500"/>
+        <p className="text-xl font-semibold text-pink-500">Foodie</p>
+      </div>
       <Routes>
         <Route path='/' element={<Meals/>}/>
         <Route path='/detail/:id' element={<Detail/>}/>
