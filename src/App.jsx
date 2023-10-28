1452f2794aa94744604378e959e4f8f9d36f85f6
koheinthurako <captainthura99@gmail.com>
2023-10-28 23:32:54 +0630

    add icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./index.css"
 import { Routes, Route } from "react-router-dom";
 import Meals from "./Meals"
 import Detail from "./Detail"
+import {IoRestaurant} from "react-icons/io5"
 
 const App = () => {
   return (
     <div className="container mx-auto">
-      
+      <IoRestaurant/>
       <Routes>
         <Route path='/' element={<Meals/>}/>
         <Route path='/detail/:id' element={<Detail/>}/>
