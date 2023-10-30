ef677b63642781c226cb4f5bda444851eba292f7
koheinthurako <captainthura99@gmail.com>
2023-10-30 22:51:34 +0630

    add note about process

--- src/Meals.jsx
+++ src/Meals.jsx
@@ -1,17 +1,18 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Meals = () => {
 
-  const [meals, setMeals] = useState([
+  const [meals, setMeals] = useState([  ]);
 
-  ]);
-
+  // axios ကိုသုံးပြီး api fetch လုပ်တယ် အပေါ်မှာ state တစ်ခုကြေညာပြီး အောက်က fetch လို့ရလာတဲ့ data တွေကို setMeals နဲ့ meals ထဲကိုထည့်ပြီး
+  // getMeals() fun ကိုခေါ်လိုက်တဲ့အခါအလုပ်လုပ်သွားပြီး နောက်ဆုံး conditional map နဲ့ loop ပတ်ပြီး rendering လုပ်သွားတယ်
   const getMeals = async () => {
     const {data} = await axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood");
     setMeals(data.meals);
     console.log(data.meals);
   }
+
   useEffect(() => {
     getMeals();
   }, [])
