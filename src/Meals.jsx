d9b60994b20065e7cd35faaf0e7370b1b8f42914
koheinthurako <captainthura99@gmail.com>
2023-10-30 22:46:00 +0630

    fetch api from themealdb

--- src/Meals.jsx
+++ src/Meals.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 const Meals = () => {
+
+  const [meals, setMeals] = useState([
+
+  ]);
+
+  const getMeals = async () => {
+    const {data} = await axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood");
+    setMeals(data.meals);
+    console.log(data.meals);
+  }
+  useEffect(() => {
+    getMeals();
+  }, [])
+
   return (
-    <div>Meals</div>
+    <div>
+      {meals?.map(meal => <h1 key={meal.idMeal}>{meal.strMeal}</h1>)}
+    </div>
   )
 }
 
