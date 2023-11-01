39a0687f39a9aa2a214744513fcba29d09a0970c
koheinthurako <captainthura99@gmail.com>
2023-11-01 22:41:04 +0630

    add youtube on details card

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import {BsYoutube} from "react-icons/bs"
 
 // ဒီ process သည် Card UI မှတစ်ဆင့် Detail သို့ data တွေ အသွားအပြန်လုပ်သည့်အတွက် သေချာကြည့်ပါ
 const Detail = () => {
@@ -17,17 +18,22 @@ const Detail = () => {
   }, [])
 
   return (
-    <div className='mt-7 mb-24'>
+    <div className='mt-7 mb-24 flex gap-12 justify-center'>
       <img src={meal.strMealThumb} alt="" width={"430px"} className='rounded-xl shadow-xl' />
-      <div className="my-5">
-        <h1 className='font-bold text-2xl mb-3'>{meal.strMeal}</h1>
-        <div className='bg-pink-500 text-white w-20 text-sm rounded-xl text-center'>{meal.strCategory}</div>
-      </div>
       <div className="">
-        <h2 className='text-xl font-semibold mb-2 underline'>Instruction</h2>
-        <p>{meal.strInstructions}</p>
+        <div className="my-5">
+          <h1 className='font-bold text-2xl mb-3'>{meal.strMeal}</h1>
+          <div className='bg-pink-500 text-white w-20 text-sm rounded-xl text-center'>{meal.strCategory}</div>
+        </div>
+        <div className='mb-5'>
+          <h2 className='text-xl font-semibold mb-2 underline'>Instruction</h2>
+          <p className=''>{meal.strInstructions}</p>
+        </div>
+        <div className="flex gap-2 items-center">
+          <a href={meal.strYoutube} target='__blank'><BsYoutube className='text-red-600 text-3xl cursor-pointer'/></a>
+          <p className='text-slate-500'>Watch on YouTube</p>
+        </div>
       </div>
-      <a></a>
     </div>
   )
 }
