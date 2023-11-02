15f2fb349dd1ba2a99ff5e1053de462aa07d2fcc
koheinthurako <captainthura99@gmail.com>
2023-11-02 21:42:10 +0630

    add card transition duration

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({meal}) => {
         <img src={meal.strMealThumb} width={"200px"} className="rounded-lg mt-2" alt="" />
         <h3>{meal.strMeal}</h3>
             <Link to={`/detail/${meal.idMeal}`} className="absolute bottom-5">
-              <button className="text-white bg-blue-500 px-10 py-2 rounded-xl hover:bg-blue-700">
+              <button className="text-white bg-blue-500 px-10 py-2 rounded-xl hover:bg-blue-700 transition duration-100">
                 <IoEyeSharp className="text-xl"/>
               </button>
             </Link>        
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <Link to={"/"}>
         <div className="flex bg-white w-36 rounded-xl shadow-lg py-2 justify-center items-center gap-3 my-8 px-5
-        transition duration-100 hover:scale-105">
+        transition duration-100 mx-5 hover:scale-105">
             <IoRestaurant className="text-2xl text-pink-500"/>
             <p className="text-xl font-semibold text-pink-500">Foodie</p>
         </div>          
