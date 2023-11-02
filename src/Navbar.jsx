fe0d9db269f7627f07f417f0a777164a399e5527
koheinthurako <captainthura99@gmail.com>
2023-11-02 21:40:05 +0630

    add logo hover css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <Link to={"/"}>
-        <div className="flex items-center gap-3 my-8 px-5">
+        <div className="flex bg-white w-36 rounded-xl shadow-lg py-2 justify-center items-center gap-3 my-8 px-5
+        transition duration-100 hover:scale-105">
             <IoRestaurant className="text-2xl text-pink-500"/>
             <p className="text-xl font-semibold text-pink-500">Foodie</p>
         </div>          
