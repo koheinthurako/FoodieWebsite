ab0188fb8b0174f270045c03e5af1466b8fdf93f
koheinthurako <captainthura99@gmail.com>
2023-11-02 22:09:13 +0630

    change about css

--- src/About.jsx
+++ src/About.jsx
@@ -13,9 +13,9 @@ const About = () => {
     <>
       {isLoading ? <Loading/> : (
         <div className='my-7 mx-5 py-3 mb-24 flex flex-col gap-2 items-center justify-center bg-slate-50 rounded-xl shadow-xl'>
-          <h1 className="font-bold text-2xl tracking-widest text-gray-500 py-5">THANK YOU FOR YOUR ATTENTION</h1>
+          <h1 className="font-bold text-lg tracking-widest text-gray-500 py-5 text-center">THANK YOU FOR YOUR ATTENTION</h1>
           <Link to={'/'}>
-            <button className="bg-pink-500 text-white px-28 py-1 my-5 rounded-xl hover:bg-pink-700 transition duration-100">Back To Home</button>
+            <button className="bg-pink-500 text-white mx-5 px-24 py-1 my-5 rounded-xl hover:bg-pink-700 transition duration-100">Back To Home</button>
           </Link>
         </div>
       )}
