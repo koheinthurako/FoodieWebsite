1718d595bf8432d1bc1d145387ad284baa215fbd
koheinthurako <captainthura99@gmail.com>
2023-11-04 21:23:26 +0630

    add footer information

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,9 @@
-
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-gray-200 py-12">
+        <h1 className="text-sm text-center text-gray-500">Copyright.All Rights Reserved</h1>
+    </footer>
+)
 }
 
 export default Footer
