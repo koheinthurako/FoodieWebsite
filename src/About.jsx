f7a493c9046c5cfebc52c94e5fa0ab75eea6d8a2
koheinthurako <captainthura99@gmail.com>
2023-11-04 21:38:30 +0630

    change direction of about card button

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,7 @@
 import Loading from "./Loader/Loading";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io"
 
 const About = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -14,8 +15,13 @@ const About = () => {
       {isLoading ? <Loading/> : (
         <div className='my-7 mx-5 py-3 mb-24 flex flex-col gap-2 items-center justify-center bg-slate-50 rounded-xl shadow-xl'>
           <h1 className="font-bold text-lg tracking-widest text-gray-500 py-5 text-center">THANK YOU FOR YOUR ATTENTION</h1>
-          <Link to={'/'}>
-            <button className="bg-pink-500 text-white mx-5 px-24 py-1 my-5 rounded-xl hover:bg-pink-700 transition duration-100">Back To Home</button>
+          <Link to={-1}>
+            <button className="bg-pink-500 text-white mx-5 px-12 py-1 my-5 rounded-xl hover:bg-pink-700 transition duration-100">
+              <div className="flex items-center gap-1 justify-center">
+                <IoIosArrowBack/>
+                <p>Back</p>
+              </div>
+            </button>
           </Link>
         </div>
       )}
--- src/Detail.jsx
+++ src/Detail.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import {BsYoutube} from "react-icons/bs"
 import Loading from './Loader/Loading';
+import Navbar from './Navbar';
 
 // ဒီ process သည် Card UI မှတစ်ဆင့် Detail သို့ data တွေ အသွားအပြန်လုပ်သည့်အတွက် သေချာကြည့်ပါ
 const Detail = () => {
@@ -24,28 +25,31 @@ const Detail = () => {
   return (
     <>
       {isLoading ? <Loading/> : (
-        <div className='my-7 mx-5 py-3 mb-24 flex flex-wrap gap-9 items-center justify-center bg-slate-50 rounded-xl shadow-xl'>
-          <img src={meal.strMealThumb} alt="" className='w-96 h-max rounded-xl shadow-xl' />
-          <div className="px-12 w-[700px]">
-            <div className="my-5">
-              <h1 className='font-bold text-2xl mb-3'>{meal.strMeal}</h1>
-              <div className='bg-pink-500 text-white w-20 text-sm rounded-xl text-center'>{meal.strCategory}</div>
-            </div>
-            <div className='mb-5'>
-              <h2 className='text-xl font-semibold mb-2 underline'>Instruction</h2>
-              <p>{meal.strInstructions}</p>
-            </div>
-            <div className="flex flex-wrap gap-12 mb-5">
-              <div className='flex gap-3 items-center'>
-                <a href={meal.strYoutube} target='__blank'><BsYoutube className='text-red-600 text-3xl cursor-pointer'/></a>
-                <a href={meal.strYoutube} target='__blank' className='text-slate-500 cursor-pointer'>Watch on YouTube</a>
+        <>
+          <Navbar/>
+          <div className='my-7 mx-5 py-3 mb-24 flex flex-wrap gap-9 items-center justify-center bg-slate-50 rounded-xl shadow-xl'>
+            <img src={meal.strMealThumb} alt="" className='w-96 h-max rounded-xl shadow-xl' />
+            <div className="px-12 w-[700px]">
+              <div className="my-5">
+                <h1 className='font-bold text-2xl mb-3'>{meal.strMeal}</h1>
+                <div className='bg-pink-500 text-white w-20 text-sm rounded-xl text-center'>{meal.strCategory}</div>
               </div>
-              <Link to={-1}>
-                <button className='bg-pink-500 text-white px-12 py-1 rounded-xl hover:bg-pink-700 transition duration-150'>Home</button>
-              </Link>
+              <div className='mb-5'>
+                <h2 className='text-xl font-semibold mb-2 underline'>Instruction</h2>
+                <p>{meal.strInstructions}</p>
+              </div>
+              <div className="flex flex-wrap gap-12 mb-5">
+                <div className='flex gap-3 items-center'>
+                  <a href={meal.strYoutube} target='__blank'><BsYoutube className='text-red-600 text-3xl cursor-pointer'/></a>
+                  <a href={meal.strYoutube} target='__blank' className='text-slate-500 cursor-pointer'>Watch on YouTube</a>
+                </div>
+                <Link to={-1}>
+                  <button className='bg-pink-500 text-white px-12 py-1 rounded-xl hover:bg-pink-700 transition duration-150'>Home</button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </>
   )
