d825c5ef74e60f6d80d239f30b52ce7214b09841
koheinthurako <captainthura99@gmail.com>
2023-11-05 22:03:48 +0630

    add card click link|

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,16 +4,19 @@ import { Link } from "react-router-dom";
 // ဟိုဘက်က rendering လုပ်ဖို့ data လှမ်းပေးလိုက်တဲ့အခါ ဒီဘက်က အောက်ကလို () ထဲမှာ meal ကို {} ထဲထည့်ပြီးပြန်သုံးပေးရတယ်
 const Card = ({meal}) => {
   return (
-    <div className="w-64 bg-slate-50 flex justify-top items-center flex-col p-5 rounded-xl gap-5 cursor-pointer
-    relative h-[400px] hover:shadow-2xl hover:scale-105 transition duration-200">
-        <img src={meal.strMealThumb} width={"200px"} className="rounded-lg mt-2" alt="" />
-        <h3>{meal.strMeal}</h3>
-            <Link to={`/detail/${meal.idMeal}`} className="absolute bottom-5">
-              <button className="text-white bg-blue-500 px-10 py-2 rounded-xl hover:bg-blue-700 transition duration-100">
-                <IoEyeSharp className="text-xl"/>
-              </button>
-            </Link>        
-    </div>
+    <Link to={`/detail/${meal.idMeal}`}>
+        <div className="w-64 bg-slate-50 flex justify-top items-center flex-col p-5 rounded-xl gap-5 cursor-pointer
+        relative h-[400px] hover:shadow-2xl hover:scale-105 transition duration-200">
+            <img src={meal.strMealThumb} width={"200px"} className="rounded-lg mt-2" alt="" />
+            <h3>{meal.strMeal}</h3>
+                <Link to={`/detail/${meal.idMeal}`} className="absolute bottom-5">
+                  <button className="text-white bg-blue-500 px-10 py-2 rounded-xl hover:bg-blue-700 transition duration-100">
+                    <IoEyeSharp className="text-xl"/>
+                  </button>
+                </Link>        
+        </div>
+
+    </Link>
   )
 }
 
