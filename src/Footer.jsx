84adb683b9ffc3fd1c74fd9ff113ce6052136d46
koheinthurako <captainthura99@gmail.com>
2023-11-05 22:09:13 +0630

    add copyright symbol

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-gray-200 py-12">
-        <h1 className="text-sm text-center text-gray-500">Copyright.All Rights Reserved</h1>
+        <h1 className="text-sm text-center text-gray-500">©Copyright.All Rights Reserved</h1>
     </footer>
 )
 }
